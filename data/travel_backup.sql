2c8aa3efb7a080e345a28cf98e24b45a592ab3d8
chaohsuan <chaohsuan@chaohsuans-MacBook.local>
2021-05-30 13:48:58 +0800

    210530v3 mysql data backup

--- data/travel_backup.sql
+++ data/travel_backup.sql
@@ -16,6 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `orderEntry`
+--
+
+DROP TABLE IF EXISTS `orderEntry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderEntry` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `order_number` varchar(255) NOT NULL COMMENT '訂單編號',
+  `bank_transaction_id` varchar(255) DEFAULT NULL,
+  `price` int NOT NULL COMMENT '訂單價格',
+  `user_id` bigint NOT NULL COMMENT '會員編號',
+  `phone` varchar(255) NOT NULL COMMENT '會員電話',
+  `attraction_id` int NOT NULL COMMENT '景點編號',
+  `date` date NOT NULL COMMENT '行程日期',
+  `time` varchar(255) NOT NULL COMMENT '行程時間',
+  `status` int NOT NULL COMMENT '交易代碼',
+  `time_established` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '訂單建立時間',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `orderentry_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `TaipeiTravel`
 --
 
@@ -63,4 +88,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-26 10:00:26
+-- Dump completed on 2021-05-30 13:18:26
